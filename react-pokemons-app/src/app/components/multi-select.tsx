6d017f1e465880efaaa85bb89bdd77c040fb2417
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-07-10 15:56:34 +0200

    Initial

---
+++ react-pokemons-app/src/app/components/multi-select.tsx
@@ -0,0 +1,54 @@
+import './multi-select.css';
+import React, { MouseEvent, useState } from 'react';
+
+type Props = {
+  items: string[];
+  selected: string[];
+  onSelect(selection: string[]): void;
+};
+
+function MultiSelect(props: Props) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  /*
+  const els = [];
+
+  for (const item of props.items) {
+    els.push(<div>{item}</div>)
+  }
+
+  const els = props.items.map((item) => <div>{item}</div>);
+  */
+
+  function handleClickValues(event: MouseEvent<HTMLDivElement>) {
+    setMenuOpen(!menuOpen);
+  }
+
+  function handleClickItem(item: string) {
+    if (props.selected.includes(item)) {
+      props.onSelect(props.selected.filter((el) => el !== item));
+    } else {
+      props.onSelect([...props.selected, item]);
+    }
+  }
+
+  return (
+    <div className="MultiSelect">
+      <div className="values" onClick={handleClickValues}>{props.selected.join(', ')}</div>
+      {menuOpen && (
+        <div className="menu">
+          {props.items.map((item) => (
+            <div className="item" key={item}>
+              <label>
+                <input type="checkbox" className="filled-in" checked={props.selected.includes(item)} onChange={() => handleClickItem(item)} />
+                <span>{item}</span>
+              </label>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default MultiSelect;
---
+++ react-pokemons-app/src/main.tsx
@@ -0,0 +1,44 @@
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+
+import App from './app/app';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+/*
+setInterval(() => {
+  // Version DOM
+  // const rootEl = document.getElementById('root') as HTMLElement;
+  // rootEl.innerHTML = '';
+  //
+  // const containerEl = document.createElement('div');
+  // const inputEl = document.createElement('input');
+  // inputEl.type = 'text';
+  //
+  // const divEl = document.createElement('div');
+  // divEl.innerText = (new Date()).toLocaleTimeString();
+  //
+  // containerEl.append(inputEl, divEl);
+  //
+  // rootEl.append(containerEl);
+
+  // Version React
+  root.render(
+    <div>
+      <input type="text" />
+      <div>
+        {(new Date()).toLocaleTimeString()}
+      </div>
+    </div>
+  );
+}, 1000);
+
+ */
